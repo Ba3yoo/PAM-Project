edbe61ea23a335e59e9b6c47143b1df5bbc82364
Ikram-sabila <ikramsabila@gmail.com>
2024-05-27 08:03:36 +0700

    names

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.projekakhir"
+    namespace = "com.example.pam_project"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.projekakhir"
+        applicationId = "com.example.pam_project"
         minSdk = 29
         targetSdk = 34
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ProjekAkhir"
+rootProject.name = "PAM-Project"
 include(":app")
  
